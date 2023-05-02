218ecbf28399b71fdfd0daaeab334f68de27e991
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-05-02 13:12:59 +0400

    connected sass

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,11 @@
+import './_vars.scss'
+import "./_reset.scss";
+import "./_base.scss";
+
 function App() {
   return (
     <div className="App">
-      
+      <h1> hello</h1>
     </div>
   );
 }
